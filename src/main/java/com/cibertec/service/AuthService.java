e0470f8b6271cf8087208dad2a5f7b389cf3b738
Angely <eusebioangely@gmail.com>
2026-06-28 01:51:50 -0500

    dasd

--- src/main/java/com/cibertec/service/AuthService.java
+++ src/main/java/com/cibertec/service/AuthService.java
@@ -1,10 +1,15 @@
 package com.cibertec.service;
 
+import java.util.List;
+
 import com.cibertec.dto.LoginRequest;
 import com.cibertec.dto.RegisterRequest;
+import com.cibertec.entity.Product;
+import com.cibertec.entity.User;
 
 public interface AuthService {
 
+	List<User> findAll();
 	void register(RegisterRequest request);
     String login(LoginRequest request);
     
